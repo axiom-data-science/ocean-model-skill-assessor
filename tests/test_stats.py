559246ee09f51ad503c3cab3fcf61cd17f51dff8
Kristen Thyng <kristen@axds.co>
2022-08-05 16:16:57 -0500

    now updated values in tests accordingly

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -23,20 +23,20 @@ class TestStats:
 
         assert isinstance(self.aligned_signals, pd.DataFrame)
         assert self.aligned_signals.shape == (5, 2)
-        assert np.isclose(self.aligned_signals["model"].mean(), 0.16391766802943322)
-        assert np.isclose(self.aligned_signals["obs"].mean(), 0.13113413442354657)
+        assert np.isclose(self.aligned_signals["model"].mean(), -0.35140932)
+        assert np.isclose(self.aligned_signals["obs"].mean(), -0.28112745)
 
     def test_align_xr(self):
 
         assert isinstance(self.aligned_signals_xr, pd.DataFrame)
         assert self.aligned_signals_xr.shape == (5, 2)
-        assert np.isclose(self.aligned_signals_xr["model"].mean(), 0.16391766802943322)
-        assert np.isclose(self.aligned_signals_xr["obs"].mean(), 0.13113413442354657)
+        assert np.isclose(self.aligned_signals_xr["model"].mean(), -0.35140932)
+        assert np.isclose(self.aligned_signals_xr["obs"].mean(), -0.28112745)
 
     def test_bias(self):
         bias = stats.compute_bias(self.obs, self.model)
 
-        assert np.isclose(bias, 0.032783533605886636)
+        assert np.isclose(bias, -0.07028185)
 
     def test_correlation_coefficient(self):
         corr_coef = stats.compute_correlation_coefficient(self.obs, self.model)
@@ -46,35 +46,35 @@ class TestStats:
     def test_index_of_agreement(self):
         ioa = stats.compute_index_of_agreement(self.obs, self.model)
 
-        assert np.isclose(ioa, 0.9872247131609245)
+        assert np.isclose(ioa, 0.9845252502709627)
 
     def test_mean_square_error(self):
         mse = stats.compute_mean_square_error(self.obs, self.model, centered=False)
 
-        assert np.isclose(mse, 0.03156566684130121)
+        assert np.isclose(mse, 0.024080737)
 
     def test_mean_square_error_centered(self):
         mse = stats.compute_mean_square_error(self.obs, self.model, centered=True)
 
-        assert np.isclose(mse, 0.03049090676561291)
+        assert np.isclose(mse, 0.019141199)
 
     def test_murphy_skill_score(self):
         mss = stats.compute_murphy_skill_score(self.obs, self.model)
 
-        assert np.isclose(mss, 0.935296965985732)
+        assert np.isclose(mss, 0.9213713780045509)
 
     def test_root_mean_square_error(self):
         rmse = stats.compute_root_mean_square_error(self.obs, self.model)
 
-        assert np.isclose(rmse, 0.17766729254790034)
+        assert np.isclose(rmse, 0.1551797)
 
     def test_descriptive_statistics(self):
         max, min, mean, std = stats.compute_descriptive_statistics(self.model, ddof=0)
 
-        assert np.isclose(max, 0.97866)
-        assert np.isclose(min, -0.906447)
-        assert np.isclose(mean, 0.039614)
-        assert np.isclose(std, 0.863681)
+        assert np.isclose(max, 0.882148)
+        assert np.isclose(min, -1.247736)
+        assert np.isclose(mean, -0.301843)
+        assert np.isclose(std, 0.757591)
 
     def test_stats(self):
         stats_output = stats.compute_stats(self.obs, self.model)

6427b471719b9718d502df8050c95db69811da52
Kristen Thyng <kthyng@gmail.com>
2023-04-26 09:58:47 -0500

    linting

--- ocean_model_skill_assessor/plot/map.py
+++ ocean_model_skill_assessor/plot/map.py
@@ -5,16 +5,17 @@ Plot map.
 from pathlib import PurePath
 from typing import Dict, Optional, Sequence, Union
 
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 from intake.catalog import Catalog
 from matplotlib.pyplot import figure
-import matplotlib.pyplot as plt
 from numpy import allclose, array, asarray
 from shapely.geometry import Polygon
 from xarray import DataArray, Dataset
-import pandas as pd
-import numpy as np
 
-from ..utils import find_bbox, open_catalogs, shift_longitudes, astype
+from ..utils import astype, find_bbox, open_catalogs, shift_longitudes
 
 
 try:
@@ -34,32 +35,32 @@ def setup_ax(ax, land_10m, left_labels=True, fontsize=12):
     )
     gl.bottom_labels = False  # turn off labels where you don't want them
     gl.right_labels = False
-    gl.xlabel_style = {'size': fontsize}
-    gl.ylabel_style = {'size': fontsize}
+    gl.xlabel_style = {"size": fontsize}
+    gl.ylabel_style = {"size": fontsize}
     if not left_labels:
         gl.left_labels = False
         gl.right_labels = True
     ax.coastlines(resolution=res)
     ax.add_feature(land_10m, facecolor="0.8")
-    
+
 
 def plot_map(
     maps: array,
     figname: Union[str, PurePath],
-    extent: Optional[Sequence] = None,
+    extent: Optional[list] = None,
     p: Optional[Polygon] = None,
     label_with_station_name: bool = False,
     dd: list = [0.0, 0.0],
     annotate: bool = True,
     annotate_fontsize: int = 12,
-    figsize: tuple[float] = (8, 7),
+    figsize: tuple[int, int] = (8, 7),
     two_maps: dict = None,
     map_font_size: int = 12,
     markersize: int = 5,
     markeredgewidth: float = 0.5,
     linewidth_data: int = 3,
     linewidth_poly: int = 2,
-    colors_data: str = col_label,
+    colors_data: Union[str, list] = col_label,
     legend: bool = False,
     loc: str = "best",
     suptitle: Optional[str] = None,
@@ -90,7 +91,7 @@ def plot_map(
         Figure size for matplotlib
     two_maps : dict
         Plot two maps side by side: presumably a zoomed-out version on the left ("extent_left") with a box showing the area enclosed by the magnified map on the right ("extent_right"). The data locations are only plotted in the right-hand map. You can also input "width_ratios" to shift the width between the map and the magnified map.
-        
+
         Example usage: ``two_maps = dict(extent_left=[1,4,1,4], extent_right=[2,3,2,3], width_ratios=[0.67, 1.33])``
     map_font_size : int
         Font size for grid labels.
@@ -131,7 +132,7 @@ def plot_map(
     min_lons, max_lons = maps[:, 0].astype(float), maps[:, 1].astype(float)
     min_lats, max_lats = maps[:, 2].astype(float), maps[:, 3].astype(float)
     station_names = maps[:, 4].astype(str)
-    
+
     # if isinstance(colors_data, str):
     #     mat = np.ones((len(min_lons)), dtype=str)
     #     mat[:] = colors_data
@@ -140,7 +141,6 @@ def plot_map(
     # else:
     #     colors_data = colors_data
 
-    
     # check for presence of type
     if maps.shape[1] > 5:
         types = maps[:, 5].astype(str)
@@ -148,7 +148,7 @@ def plot_map(
     central_longitude = min_lons.mean()
     proj = cartopy.crs.Mercator(central_longitude=float(central_longitude))
 
-    fig = figure(figsize=figsize, dpi=100)    
+    fig = figure(figsize=figsize, dpi=100)
     if two_maps is not None:
         col_box = "#DE3163"
         # set up larger map
@@ -156,9 +156,12 @@ def plot_map(
             width_ratios = two_maps["width_ratios"]
         else:
             width_ratios = [1, 1]
-        ax_map, ax = fig.subplots(1, 2, width_ratios=width_ratios, 
-                                  subplot_kw=dict(projection=proj,
-                                                  frameon=False))
+        ax_map, ax = fig.subplots(
+            1,
+            2,
+            width_ratios=width_ratios,
+            subplot_kw=dict(projection=proj, frameon=False),
+        )
         setup_ax(ax_map, land_10m, fontsize=map_font_size)
         ax_map.set_extent(two_maps["extent_left"], pc)
 
@@ -166,30 +169,40 @@ def plot_map(
 
         # add box
         import matplotlib.patches as mpatches
+
         extent = two_maps["extent_right"]
-        ax_map.add_patch(mpatches.Rectangle(xy=[extent[0], extent[2]], 
-                                            width=extent[1]-extent[0],   
-                                            height=extent[3]-extent[2],
-                                            facecolor='none',
-                                            # alpha=0.5,
-                                            lw=2,
-                                            edgecolor = col_box,
-                                            transform=pc,
-                                            zorder=10),)
+        assert isinstance(extent, list)
+        ax_map.add_patch(
+            mpatches.Rectangle(
+                xy=[extent[0], extent[2]],
+                width=extent[1] - extent[0],
+                height=extent[3] - extent[2],
+                facecolor="none",
+                # alpha=0.5,
+                lw=2,
+                edgecolor=col_box,
+                transform=pc,
+                zorder=10,
+            ),
+        )
 
         # set up magnified map, which will be used for the rest of the function
         ax = fig.add_subplot(1, 2, 2, projection=proj)
         setup_ax(ax, land_10m, left_labels=False, fontsize=map_font_size)
         # add box to magnified plot to emphasize connection
-        ax.add_patch(mpatches.Rectangle(xy=[extent[0], extent[2]], 
-                                            width=extent[1]-extent[0],   
-                                            height=extent[3]-extent[2],
-                                            facecolor='none',
-                                            # alpha=0.5,
-                                            lw=4,
-                                            edgecolor = col_box,
-                                            transform=pc,
-                                            zorder=10),)
+        ax.add_patch(
+            mpatches.Rectangle(
+                xy=[extent[0], extent[2]],
+                width=extent[1] - extent[0],
+                height=extent[3] - extent[2],
+                facecolor="none",
+                # alpha=0.5,
+                lw=4,
+                edgecolor=col_box,
+                transform=pc,
+                zorder=10,
+            ),
+        )
 
     else:
         ax = fig.add_axes([0.06, 0.01, 0.93, 0.95], projection=proj)
@@ -199,11 +212,17 @@ def plot_map(
     if p is not None:
         # this needs to be checked for resulting type and order
         bbox = p.bounds
-        ax.add_geometries([p], crs=pc, facecolor="none", edgecolor="r", linestyle="-",
-                          linewidth=linewidth_poly)
+        ax.add_geometries(
+            [p],
+            crs=pc,
+            facecolor="none",
+            edgecolor="r",
+            linestyle="-",
+            linewidth=linewidth_poly,
+        )
     else:
         bbox = [min(min_lons), min(min_lats), max(max_lons), max(max_lats)]
-    
+
     kwargs_plot = {}
 
     # plot stations
@@ -213,6 +232,7 @@ def plot_map(
             kwargs_plot["label"] = list(station_names[inds])
             ax.set_prop_cycle(color=colors_data)
         else:
+            assert isinstance(colors_data, list)
             kwargs_plot["color"] = colors_data
         # # import pdb; pdb.set_trace()
         # ax.scatter(
@@ -228,7 +248,7 @@ def plot_map(
         #     **kwargs_plot,
         # )
         ax.plot(
-             [min_lons[inds]],
+            [min_lons[inds]],
             #  min_lons[inds][:,np.newaxis],
             # min_lons[inds],
             [min_lats[inds]],
@@ -243,7 +263,7 @@ def plot_map(
             ls="",
             **kwargs_plot,
         )
-    
+
     # plot lines
     inds = types == "line"
     if inds.sum() > 0:
@@ -251,7 +271,8 @@ def plot_map(
             kwargs_plot["label"] = list(station_names[inds])
             ax.set_prop_cycle(color=colors_data)
         else:
-            kwargs_plot["color"] = colors_data#[inds]
+            assert isinstance(colors_data, list)
+            kwargs_plot["color"] = colors_data  # [inds]
         ax.plot(
             [min_lons[inds], max_lons[inds]],
             [min_lats[inds], max_lats[inds]],
@@ -260,7 +281,7 @@ def plot_map(
             transform=pc,
             **kwargs_plot,
         )
-    
+
     # plot boxes
     inds = types == "box"
     if inds.sum() > 0:
@@ -268,10 +289,23 @@ def plot_map(
             kwargs_plot["label"] = list(station_names[inds])
             ax.set_prop_cycle(color=colors_data)
         else:
-            kwargs_plot["color"] = colors_data#[inds]
+            assert isinstance(colors_data, list)
+            kwargs_plot["color"] = colors_data  # [inds]
         ax.plot(
-            [min_lons[inds], max_lons[inds], max_lons[inds], min_lons[inds], min_lons[inds]],
-            [min_lats[inds], min_lats[inds], max_lats[inds], max_lats[inds], min_lats[inds]],
+            [
+                min_lons[inds],
+                max_lons[inds],
+                max_lons[inds],
+                min_lons[inds],
+                min_lons[inds],
+            ],
+            [
+                min_lats[inds],
+                min_lats[inds],
+                max_lats[inds],
+                max_lats[inds],
+                min_lats[inds],
+            ],
             linestyle="-",
             linewidth=linewidth_data,
             transform=pc,
@@ -283,8 +317,8 @@ def plot_map(
     if not legend or annotate:
         if not isinstance(dd, list):
             raise ValueError("dd must be a list of distance in [x,y].")
-        if isinstance(dd[0], (int,float)):
-            dds = [dd]*len(min_lons)
+        if isinstance(dd[0], (int, float)):
+            dds = [dd] * len(min_lons)
         else:
             dds = dd
         for i, (lon, lat, dd) in enumerate(zip(min_lons, min_lats, dds)):
@@ -293,7 +327,13 @@ def plot_map(
             else:
                 label = i
             xyproj = np.array(ax.projection.transform_point(lon, lat, pc)) + dd
-            ax.annotate(label, xy=xyproj, xytext=xyproj, color=col_label, fontsize=annotate_fontsize)
+            ax.annotate(
+                label,
+                xy=xyproj,
+                xytext=xyproj,
+                color=col_label,
+                fontsize=annotate_fontsize,
+            )
 
     # [min lon, max lon, min lat, max lat]
     if extent is None:
@@ -325,17 +365,17 @@ def plot_map(
             ]
 
     ax.set_extent(extent_use, pc)
-    
+
     if legend:
         ax.legend(loc=loc, fontsize=annotate_fontsize)
         ax.set_prop_cycle(color=colors_data)
-    
+
     if suptitle is not None:
         plt.suptitle(suptitle, fontsize=suptitle_fontsize)
 
     if two_maps is not None and tight_layout:
         fig.tight_layout()
-    
+
     fig.savefig(figname, dpi=100, bbox_inches="tight")
 
 
@@ -343,7 +383,7 @@ def plot_cat_on_map(
     catalog: Union[Catalog, str],
     project_name: str,
     figname: Optional[str] = None,
-    remove_duplicates = None,
+    remove_duplicates=None,
     **kwargs_map,
 ):
     """Plot catalog on map with optional model domain polygon.
@@ -371,7 +411,7 @@ def plot_cat_on_map(
 
     figname = figname or f"map_of_{cat.name}"
 
-    kwargs_map: Dict
+    # kwargs_map: Dict
 
     maps = []
     maps.extend(
@@ -384,13 +424,14 @@ def plot_cat_on_map(
                 s,
                 cat[s].metadata["maptype"] or "",
             ]
-            for s in list(cat) if "minLongitude" in cat[s].metadata
+            for s in list(cat)
+            if "minLongitude" in cat[s].metadata
         ]
     )
     # import pdb; pdb.set_trace()
-    
+
     maps = asarray(maps)
-    
+
     if remove_duplicates is not None:
         maps = remove_duplicates(maps)
 
